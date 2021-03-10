46f18c83d06f2cc9c1d55f92dcb3a863687fd17f
pavlushaaa <71553886+pavlushaaa@users.noreply.github.com>
2021-03-10 16:26:34 +0200

    [script added]

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
 import random
+
+chars = "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM1234567890,<>.?:;{[}]-_+=!@#$%^&*()"
+while True:
+    password_len = int(input("How much charachters?"))
+    password_count = int(input("how much count?"))
+    for x in range(0, password_count):
+        password = ""
+        for x in range(0, password_len):
+            password_char = random.choice(chars)
+            password = password + password_char
+        print(password)
